af632e11e199642b4e98056d369a36dc8f872b7f
natarajsahoo <nataraj@phoenixrobotix.com>
2021-06-18 20:02:55 +0000

    test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-console.log('hello from npx');
-// const moment = require('moment-timezone');
+// console.log('hello from npx');
+const moment = require('moment-timezone');
 
-// console.log('current time -> ', moment().unix());
+console.log('current time -> ', moment().unix());
